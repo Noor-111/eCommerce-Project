adf1be65963cf8e79c14283914b62e989c1058de
nobaba <noor-u-din.baba@capgemini.com>
2020-12-11 13:56:26 +0530

    [Pagination] Add support for ng-bootstrap

--- 03-Angular-frontend/ecommerce-app/src/app/app.module.ts
+++ 03-Angular-frontend/ecommerce-app/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { Routes, RouterModule} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 const routes: Routes = [
   {path: 'products/:id', component: ProductDetailComponent},
   {path: 'search/:keyword', component: ProductListComponent},
@@ -31,7 +31,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
