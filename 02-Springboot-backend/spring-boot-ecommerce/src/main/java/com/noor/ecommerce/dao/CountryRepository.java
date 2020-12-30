e4bc8c6546a14ec656a637df200f42180b9d3154
nobaba <noor-u-din.baba@capgemini.com>
2020-12-30 16:21:38 +0530

    JPA Repository for Country and State

---
+++ 02-Springboot-backend/spring-boot-ecommerce/src/main/java/com/noor/ecommerce/dao/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.noor.ecommerce.dao;
+
+import com.noor.ecommerce.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin()
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
