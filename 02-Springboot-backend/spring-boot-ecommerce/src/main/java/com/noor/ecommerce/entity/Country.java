878c71b9fec44b30bf9f6f97ab7fee3a1689e072
nobaba <noor-u-din.baba@capgemini.com>
2020-12-30 15:27:16 +0530

    JPA entities for Country and State

---
+++ 02-Springboot-backend/spring-boot-ecommerce/src/main/java/com/noor/ecommerce/entity/Country.java
@@ -0,0 +1,27 @@
+package com.noor.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table (name="country")
+@Getter
+@Setter
+public class Country {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+
+    @Column (name = "code")
+    private String code;
+
+    @Column (name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+}
