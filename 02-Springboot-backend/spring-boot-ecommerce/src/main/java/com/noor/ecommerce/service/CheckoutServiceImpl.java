d5ee728c87fbb08b92b995ce3269fdf381b0aec5
E606287 <noor-u-din.baba@capgemini.com>
2021-04-25 12:58:03 +0530

    [Save Order] Add Controller Place Order

--- 02-Springboot-backend/spring-boot-ecommerce/src/main/java/com/noor/ecommerce/service/CheckoutServiceImpl.java
+++ 02-Springboot-backend/spring-boot-ecommerce/src/main/java/com/noor/ecommerce/service/CheckoutServiceImpl.java
@@ -3,7 +3,14 @@ package com.noor.ecommerce.service;
 import com.noor.ecommerce.dao.CustomerRepository;
 import com.noor.ecommerce.dto.Purchase;
 import com.noor.ecommerce.dto.PurchaseResponse;
+import com.noor.ecommerce.entity.Customer;
+import com.noor.ecommerce.entity.Order;
+import com.noor.ecommerce.entity.OrderItem;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -15,7 +22,36 @@ public class CheckoutServiceImpl implements CheckoutService {
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        // retrieve the order from dto
+        Order order = purchase.getOrder();
+
+        // generate orderTrackingNumber
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(orderItem -> order.add(orderItem));
+
+        // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        // save to db
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+        return UUID.randomUUID().toString();
     }
 }
