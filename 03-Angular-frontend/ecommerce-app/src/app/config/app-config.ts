e744831f0df212b6d3550972dc29483f77228bc0
E606287 <noor-u-din.baba@capgemini.com>
2021-05-02 13:06:59 +0530

    [AUTH] Add okta openIDConnect config

---
+++ 03-Angular-frontend/ecommerce-app/src/app/config/app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc :{
+        clientId: '0oaoa31u7CxoeMGKX5d6',
+        issuer: 'https://dev-17624362.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email'] 
+    }
+}
